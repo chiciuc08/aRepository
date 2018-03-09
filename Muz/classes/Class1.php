09b63d9ba982a1bf0d7fadaac357d91a9cc4dea2
Chiciuc Costel <chiciuc_costel2001@yahoo.com>
2018-03-08 21:07:54 -0800

    add folders

---
+++ Muz/classes/Class1.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class1 short summary.
+ *
+ * Class1 description.
+ *
+ * @version 1.0
+ * @author Costy
+ */
+class Class1
+{
+    public function run(){
+        echo "run()dagad";
+    }
+}
